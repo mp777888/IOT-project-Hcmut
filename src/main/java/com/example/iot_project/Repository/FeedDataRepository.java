7ccb0d0c3b7ed445e81083f7bc1e09df3111e1c4
Khoa1204HCMUT <156278110+mp777888@users.noreply.github.com>
2025-03-18 13:52:45 +0700

    update api for soil moisture and light sensor, delete what unused

--- src/main/java/com/example/iot_project/Repository/FeedDataRepository.java
+++ src/main/java/com/example/iot_project/Repository/FeedDataRepository.java
@@ -3,11 +3,14 @@ package com.example.iot_project.Repository;
 import com.example.iot_project.Entity.FeedData;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface FeedDataRepository extends MongoRepository<FeedData, String> {
+    // Tìm theo feedName và sắp xếp theo timestamp giảm dần
     List<FeedData> findByFeedNameOrderByTimestampDesc(String feedName);
-    List<FeedData> findByFeedNameAndTimestampBetween(String feedName, long startTime, long endTime);
-    List<FeedData> findByTimestampBetween(long startTime, long endTime);
-    List<String> findDistinctFeedNameBy();
+    // Tìm theo feedName và khoảng thời gian
+    List<FeedData> findByFeedNameAndTimestampBetween(String feedName, LocalDateTime startTime, LocalDateTime endTime);
+    // Tìm theo khoảng thời gian
+    List<FeedData> findByTimestampBetween(LocalDateTime startTime, LocalDateTime endTime);
 }
